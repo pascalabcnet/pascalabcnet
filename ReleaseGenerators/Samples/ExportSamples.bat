2063e70d6c50c981f4a26efefb0295b4059eb75d
Бондарев Иван <ibond84@googlemail.com>
2015-05-14 23:29:27 +0200

    pnet deleted temporary files deleted

--- ReleaseGenerators/Samples/ExportSamples.bat
+++ ReleaseGenerators/Samples/ExportSamples.bat
@@ -1,8 +1,6 @@
 del Pas /s /q
 del BF /s  /q
 del PL0 /s /q
-del PNET /s /q
 GetSamples.exe "..\..\InstallerSamples" Pas\
 GetSamples.exe _svn\BF BF\
 GetSamples.exe _svn\PL0\ PL0\
-GetSamples.exe _svn\PNET\ PNET\
