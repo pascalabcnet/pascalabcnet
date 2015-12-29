f6f5d75f649cf69403ac570c59868f0b7f04210a
Бондарев Иван <ibond84@googlemail.com>
2015-12-29 18:10:38 +0100

    Удалены старые грамматики GoldParser-а

--- Grammars/PascalABC.NET/compile_PGT.bat
+++
@@ -1,4 +0,0 @@
-del PABCParser.cs 
-start /wait ..\..\utils\gpbcmd\createskelprog_main PascalABC.cgt PascalABC.pgt PascalABC.tmpl
-..\..\grmCommentCompiler\bin\debug\grmCommentCompiler PascalABC.grm PascalABC.tmpl PascalABC.cs
-copy PascalABC.cs ..\..\Parsers\PascalABCParser\pascalabc_lrparser_rules.cs
--- Grammars/PascalABC.NET/compile_RES.bat
+++
@@ -1,8 +0,0 @@
-echo Compile: CGT to RES...
-..\..\utils\ResXMaker\ResXMaker.exe PascalABC.CGT PascalABCLanguage PascalABCLang.resources
-echo.
-
-copy PascalABCLang.resources ..\..\Parsers\PascalABCParser\PascalABCLang.resources
-copy PascalABCLang.resources ..\..\Parsers\PascalABCPartParser\PascalABCLang.resources
-copy PascalABC.cs ..\..\Parsers\PascalABCParser\pascalabc_lrparser_rules.cs
-copy PascalABC_part.cs ..\..\Parsers\PascalABCPartParser\_COPY\pascalabc_lrparser_rules.cs
