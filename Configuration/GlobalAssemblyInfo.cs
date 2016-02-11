eab56c3cbc77f59c8a94391670368530d1ad3d32
miks1965 <miks@math.sfedu.ru>
2016-02-11 23:45:36 +0300

    Релиз 1164

--- Configuration/GlobalAssemblyInfo.cs
+++ Configuration/GlobalAssemblyInfo.cs
@@ -15,7 +15,7 @@ internal static class RevisionClass
 	public const string Major = "3";
 	public const string Minor = "0";
 	public const string Build = "0";
-	public const string Revision = "1163";
+	public const string Revision = "1164";
 
         public const string MainVersion = Major + "." + Minor;
 	public const string FullVersion = Major + "." + Minor + "." + Build + "." + Revision;
