c284bd34c6688c327f40beef9e69a90b9f91b1ed
Бондарев Иван <ibond84@googlemail.com>
2016-04-03 20:03:12 +0200

    Удалил GoldParserEngine.pas и BFSystem.pas из системных модулей (оставлены в TestSuite)

--- ReleaseGenerators/PABCRtl/PABCRtl32.pas
+++ ReleaseGenerators/PABCRtl/PABCRtl32.pas
@@ -2,8 +2,8 @@ library PABCRtl32;
 
 uses 
   PABCSystem, ABCButtons, ABCHouse, ABCObjects,
-  ABCSprites, BFSystem, Colors, CRT, DMCollect, DMTaskMaker, DMZadan, Drawman, 
-  DrawManField, Events, FilesOperations, GOLDParserEngine, GraphABC, 
+  ABCSprites, Colors, CRT, DMCollect, DMTaskMaker, DMZadan, Drawman, 
+  DrawManField, Events, FilesOperations, GraphABC, 
   GraphABCHelper, IniFile, PointerTools, PointRect, Robot, RobotField,
   RobotTaskMaker, RobotZadan, Sockets, Utils, VCL, Timers, PT4, PT4TaskMakerNET, RBDMUtils,
   FormsABC, Collections, Arrays, ClientServer, __RedirectIOMode, __RunMode, __RtlUtils;
--- ReleaseGenerators/PascalABCNET_version.nsh
+++ ReleaseGenerators/PascalABCNET_version.nsh
@@ -1,1 +1,1 @@
-!define VERSION '3.1.0.1207'
+!define VERSION '3.1.0.1209'
