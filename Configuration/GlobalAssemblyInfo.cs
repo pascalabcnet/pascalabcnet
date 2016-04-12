24a92cb5ce0d7000a2337c7a7d97a3d6ff21d6ed
miks1965 <miks@math.sfedu.ru>
2016-04-12 21:49:13 +0300

    версия 3.0.0.1218

--- Configuration/GlobalAssemblyInfo.cs
+++ Configuration/GlobalAssemblyInfo.cs
@@ -15,7 +15,7 @@ internal static class RevisionClass
 	public const string Major = "3";
 	public const string Minor = "1";
 	public const string Build = "0";
-	public const string Revision = "1215";
+	public const string Revision = "1218";
 
         public const string MainVersion = Major + "." + Minor;
 	public const string FullVersion = Major + "." + Minor + "." + Build + "." + Revision;
--- ReleaseGenerators/PascalABCNET_version.nsh
+++ ReleaseGenerators/PascalABCNET_version.nsh
@@ -1,1 +1,1 @@
-!define VERSION '3.1.0.1215'
+!define VERSION '3.1.0.1218'
