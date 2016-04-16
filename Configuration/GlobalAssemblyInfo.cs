7bee30b7df770c7dd45414a9e2e5927c6260974c
miks1965 <stanislav.mikhalkovich@gmail.com>
2016-04-16 19:39:53 +0300

    rr

--- Configuration/GlobalAssemblyInfo.cs
+++ Configuration/GlobalAssemblyInfo.cs
@@ -15,11 +15,7 @@ internal static class RevisionClass
 	public const string Major = "3";
 	public const string Minor = "0";
 	public const string Build = "0";
-<<<<<<< HEAD
-	public const string Revision = "1147";
-=======
 	public const string Revision = "1150";
->>>>>>> 68c8ead5cd0d14b761b6542967ac00b1012e9e71
 
         public const string MainVersion = Major + "." + Minor;
 	public const string FullVersion = Major + "." + Minor + "." + Build + "." + Revision;
