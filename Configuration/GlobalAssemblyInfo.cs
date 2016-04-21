799ac2d26487957163066631f227ee786bde6808
miks1965 <stanislav.mikhalkovich@gmail.com>
2016-04-21 12:08:38 +0300

    trivial changes

--- Configuration/GlobalAssemblyInfo.cs
+++ Configuration/GlobalAssemblyInfo.cs
@@ -13,9 +13,9 @@ using System.Runtime.CompilerServices;
 internal static class RevisionClass
 {
 	public const string Major = "3";
-	public const string Minor = "1";
+	public const string Minor = "0";
 	public const string Build = "0";
-	public const string Revision = "1218";
+	public const string Revision = "1148";
 
         public const string MainVersion = Major + "." + Minor;
 	public const string FullVersion = Major + "." + Minor + "." + Build + "." + Revision;
