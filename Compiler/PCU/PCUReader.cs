48340c1a8ca465a19f0606392d0ae77b0927ff72
Бондарев Иван <ibond84@googlemail.com>
2016-07-31 16:28:30 +0200

    bug fix #260

--- Compiler/PCU/PCUReader.cs
+++ Compiler/PCU/PCUReader.cs
@@ -287,7 +287,7 @@ namespace PascalABCCompiler.PCU
                 ChangeState(this, PCUReaderWriterState.EndReadTree, unit);
                 return unit;
             }
-            catch (Exception)
+            catch (Exception ex)
             {
                 CloseUnit();
                 throw;
@@ -476,7 +476,11 @@ namespace PascalABCCompiler.PCU
                     
                     type_node tn = GetSpecialTypeReference(names[i].offset);
                     if (tn is compiled_type_node)
+                    {
+                        if ((tn as compiled_type_node).scope == null)
+                            (tn as compiled_type_node).init_scope();
                         (tn as compiled_type_node).scope.AddSymbol(names[i].name, si);
+                    }
                     else if (tn is generic_instance_type_node)
                         tn.Scope.AddSymbol(names[i].name, si);
                     else if (tn is common_type_node)
