c92984982eeb5d2994f286824ed7f6f15e8f1c4f
miks1965 <miks@math.sfedu.ru>
2016-08-28 21:11:34 +0300

    SyntaxTree/tree/FillParentNodeVisitor.cs

---
+++ SyntaxTree/tree/FillParentNodeVisitor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PascalABCCompiler.SyntaxTree 
+{
+    public class FillParentNodeVisitor: CollectUpperNodesVisitor
+    {
+        public static FillParentNodeVisitor New
+        {
+            get
+            {
+                return new FillParentNodeVisitor();
+            }
+        }
+        public override void Enter(syntax_tree_node st)
+        {
+            base.Enter(st);
+            st.Parent = UpperNode();
+        }
+    }
+}
