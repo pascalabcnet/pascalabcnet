20d7e9ffccf97898fb864fd75c224fa33acbc466
Бондарев Иван <ibond84@googlemail.com>
2016-12-06 21:28:49 +0100

    .

--- TestSuite/issue314.pas
+++
@@ -1,9 +0,0 @@
-function fff <T1>(a: System.Tuple<T1,T1>): boolean; where T1:IComparable<T1>; 
-begin
-  Result := a.Item1.CompareTo(a.Item2)<0;
-end;
-
-begin
-  var date1 := (23,2);
-  assert(fff(date1) = false);
-end.
