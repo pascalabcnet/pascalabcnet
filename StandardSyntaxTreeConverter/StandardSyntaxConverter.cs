342a914d3952a12da0481291c909e344306c229d
miks1965 <miks@math.sfedu.ru>
2017-01-12 13:51:16 +0300

    StandardSyntaxConverter - поменял имя

---
+++ StandardSyntaxTreeConverter/StandardSyntaxConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PascalABCCompiler.SyntaxTree;
+
+namespace PascalABCCompiler.SyntaxTreeConverters
+{
+    public class StandardSyntaxTreeConverter: ISyntaxTreeConverter
+    {
+        public string Name { get; } = "Standard";
+        public syntax_tree_node Convert(syntax_tree_node root)
+        {
+            // Прошивание ссылками на Parent nodes. Должно идти первым
+            FillParentNodeVisitor.New.ProcessNode(root);
+
+            // Выносим выражения с лямбдами из заголовка foreach
+            StandOutExprWithLambdaInForeachSequenceVisitor.New.ProcessNode(root); 
+
+            return root;
+        }
+    }
+}
