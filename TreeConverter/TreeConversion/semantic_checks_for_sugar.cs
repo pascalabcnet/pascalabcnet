ac9f0221b08f7eb3157ec862a71ef729788642c5
miks1965 <miks@math.sfedu.ru>
2017-01-20 21:49:07 +0300

    semantic_checks_for_sugar.cs

---
+++ TreeConverter/TreeConversion/semantic_checks_for_sugar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using PascalABCCompiler.SyntaxTree;
+using PascalABCCompiler.TreeRealization;
+
+namespace PascalABCCompiler.TreeConverter
+{
+    public partial class syntax_tree_visitor
+    {
+        public void semantic_check_sugared_statement(SyntaxTree.assign_tuple assvartup) // подходит и для assign_var_tuple
+        {
+            // Проверить, что справа - Tuple
+            var expr = convert_strong(assvartup.expr);
+            expr = convert_if_typed_expression_to_function_call(expr);
+
+            var t = ConvertSemanticTypeNodeToNETType(expr.type);
+            if (t == null)
+                AddError(expr.location, "TUPLE_EXPECTED");
+
+            if (!t.FullName.StartsWith("System.Tuple"))
+                AddError(expr.location, "TUPLE_EXPECTED");
+
+            var n = assvartup.vars.variables.Count();
+            if (n > t.GetGenericArguments().Count())
+                AddError(get_location(assvartup.vars), "TOO_MANY_ELEMENTS_ON_LEFT_SIDE_OF_TUPLE_ASSIGNMRNT");
+        }
+    }
+}
