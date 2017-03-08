d8fd72004321c069c29430ed7ae68e903cc8cbd3
Бондарев Иван <ibond84@googlemail.com>
2017-03-08 20:46:19 +0100

    bug fix #363

--- bin/Lib/PABCSystem.pas
+++ bin/Lib/PABCSystem.pas
@@ -1217,6 +1217,24 @@ function Odd(i: uint64): boolean;
 function Cplx(re,im: real): Complex;
 /// Конструирует комплексное число по полярным координатам
 function CplxFromPolar(magnitude,phase: real) : Complex;
+/// Возвращает квадратный корень из комплексного числа
+function Sqrt(c: Complex): Complex;
+/// Возвращает модуль комплексного числа
+function Abs(c: Complex): Complex;
+/// Возвращает комплексно сопряженное число
+function Conjugate(c: Complex): Complex;
+/// Возвращает косинус комплексного числа
+function Cos(c: Complex): Complex;
+/// Возвращает экспоненту комплексного числа
+function Exp(c: Complex): Complex;
+/// Возвращает натуральный логарифм комплексного числа
+function Log(c: Complex): Complex;
+/// Возвращает десятичный логарифм комплексного числа
+function Log10(c: Complex): Complex;
+/// Возвращает степень комплексного числа
+function Power(c,power: Complex): Complex;
+/// Возвращает синус комплексного числа
+function Sin(c: Complex): Complex;
 
 // -----------------------------------------------------
 //>>     Процедуры для работы со стандартными множествами # Subroutines for set of T
@@ -7058,6 +7076,24 @@ end;
 function Cplx(re,im: real) := new Complex(re,im);
 
 function CplxFromPolar(magnitude,phase: real) := Complex.FromPolarCoordinates(magnitude,phase);
+
+function Sqrt(c: Complex) := Complex.Sqrt(c);
+
+function Abs(c: Complex) := Complex.Abs(c);
+
+function Conjugate(c: Complex) := Complex.Conjugate(c);
+
+function Cos(c: Complex) := Complex.Cos(c);
+
+function Exp(c: Complex) := Complex.Exp(c);
+
+function Log(c: Complex) := Complex.Log(c);
+
+function Log10(c: Complex) := Complex.Log10(c);
+
+function Power(c,power: Complex) := Complex.Pow(c,power);
+
+function Sin(c: Complex) := Complex.Sin(c);
 
 // -----------------------------------------------------
 //                Dynamic arrays: implementation
