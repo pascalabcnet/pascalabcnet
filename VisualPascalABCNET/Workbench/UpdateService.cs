e0d9b435517bafff550dcb7789964255f46a9160
Бондарев Иван <ibond84@googlemail.com>
2017-04-07 21:11:18 +0200

    fix #374

--- VisualPascalABCNET/Workbench/UpdateService.cs
+++ VisualPascalABCNET/Workbench/UpdateService.cs
@@ -21,11 +21,13 @@ namespace VisualPascalABC
         public void CheckForUpdates()
         {
             int status = 1;//1 - up to date, 0 - not up to date, -1 error
+            string newVersion = null;
+            string curVersion = null;
             try
             {
                 WebClient client = new WebClient();
-                string newVersion = client.DownloadString("http://pascalabc.net/downloads/pabcversion.txt").Trim();
-                string curVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+                newVersion = client.DownloadString("http://pascalabc.net/downloads/pabcversion.txt").Trim();
+                curVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
                 if ((new Version(curVersion)).CompareTo(new Version(newVersion)) == -1)
                     status = 0;
             }
@@ -39,7 +41,11 @@ namespace VisualPascalABC
                     MessageBox.Show(PascalABCCompiler.StringResources.Get("VP_MF_VERSION_IS_UP_DO_DATE"), PascalABCCompiler.StringResources.Get("VP_MF_UPDATE_CHECK"), MessageBoxButtons.OK);
                     break;
                 case 0:
-                    if (MessageBox.Show(PascalABCCompiler.StringResources.Get("VP_MF_UPDATE_AVAILABLE"), PascalABCCompiler.StringResources.Get("VP_MF_UPDATE_CHECK"), MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    if (MessageBox.Show(PascalABCCompiler.StringResources.Get("VP_MF_UPDATE_AVAILABLE") + Environment.NewLine + 
+                        string.Format(PascalABCCompiler.StringResources.Get("VP_MF_UPDATE_AVAILABLE_CURRENT_VESION{0}"), curVersion) + Environment.NewLine +
+                        string.Format(PascalABCCompiler.StringResources.Get("VP_MF_UPDATE_AVAILABLE_NEW_VESION{0}"), newVersion), 
+                        PascalABCCompiler.StringResources.Get("VP_MF_UPDATE_CHECK"), 
+                        MessageBoxButtons.YesNo) == DialogResult.Yes)
                     {
                         WorkbenchServiceFactory.OperationsService.AddTabWithUrl("PascalABC.NET", PascalABCCompiler.StringResources.Get("VP_MF_PABC_DOWNLOAD_PAGE"));
                     }
