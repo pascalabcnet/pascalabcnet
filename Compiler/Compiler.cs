74c712afdfb51ee5559d6d53daa5802d91f6233f
Бондарев Иван <ibond84@googlemail.com>
2017-05-22 19:53:52 +0200

    bug fix #400

--- Compiler/Compiler.cs
+++ Compiler/Compiler.cs
@@ -1711,23 +1711,23 @@ namespace PascalABCCompiler
         }
         
         private ProjectInfo project;
-        
+
         private void PrepareCompileOptionsForProject()
         {
-        	project = new ProjectInfo();
-        	project.Load(CompilerOptions.SourceFileName);
-        	//LoadProject(CompilerOptions.SourceFileName);
-        	switch (project.ProjectType)
-        	{
-        		case ProjectType.ConsoleApp : CompilerOptions.OutputFileType = CompilerOptions.OutputType.ConsoleApplicaton; break;
-        		case ProjectType.WindowsApp : CompilerOptions.OutputFileType = CompilerOptions.OutputType.WindowsApplication; break;
-        		case ProjectType.Library : CompilerOptions.OutputFileType = CompilerOptions.OutputType.ClassLibrary; break;
-        	}
-        	CompilerOptions.SourceFileName = project.main_file;
-        	CompilerOptions.Debug = project.include_debug_info;
-        	CompilerOptions.OutputFileName = project.output_file_name;
-        	CompilerOptions.OutputDirectory = project.output_directory;
-            
+            project = new ProjectInfo();
+            project.Load(CompilerOptions.SourceFileName);
+            //LoadProject(CompilerOptions.SourceFileName);
+            switch (project.ProjectType)
+            {
+                case ProjectType.ConsoleApp: CompilerOptions.OutputFileType = CompilerOptions.OutputType.ConsoleApplicaton; break;
+                case ProjectType.WindowsApp: CompilerOptions.OutputFileType = CompilerOptions.OutputType.WindowsApplication; break;
+                case ProjectType.Library: CompilerOptions.OutputFileType = CompilerOptions.OutputType.ClassLibrary; break;
+            }
+            CompilerOptions.SourceFileName = project.main_file;
+            CompilerOptions.Debug = project.include_debug_info;
+            CompilerOptions.OutputFileName = project.output_file_name;
+
+            CompilerOptions.OutputDirectory = project.output_directory;
         }
         
 		public string Compile()
