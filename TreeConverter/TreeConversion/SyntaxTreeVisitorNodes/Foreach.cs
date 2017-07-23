a7d9fea8453cdafac4ebb9d88bda03b25463016e
Voloshin Bogdan <ya.bovi2012@yandex.ru>
2017-07-23 19:21:30 +0300

    Refactor foreach

--- TreeConverter/TreeConversion/SyntaxTreeVisitorNodes/Foreach.cs
+++ TreeConverter/TreeConversion/SyntaxTreeVisitorNodes/Foreach.cs
@@ -27,6 +27,8 @@ namespace PascalABCCompiler.TreeConverter
     public partial class syntax_tree_visitor // SSM 02.01.17 менять на визитор с другим порядком обхода можно, но бессмысленно
     {
 
+
+
         public override void visit(SyntaxTree.foreach_stmt _foreach_stmt)
         {
             var lambdaSearcher = new LambdaSearcher(_foreach_stmt.in_what);
@@ -52,36 +54,7 @@ namespace PascalABCCompiler.TreeConverter
             if (!FindIEnumerableElementType(foreachCollection.type, ref elem_type, out sys_coll_ienum))
                 AddError(foreachCollection.location, "CAN_NOT_EXECUTE_FOREACH_BY_EXPR_OF_TYPE_{0}", foreachCollection.type.name);
 
-            var_definition_node foreachVariable = null;
-            if (_foreach_stmt.type_name == null) // значит, переменная определена в другом месте
-            {
-                location loc1 = get_location(_foreach_stmt.identifier);
-                definition_node dn = context.check_name_node_type(_foreach_stmt.identifier.name, loc1,
-                    general_node_type.variable_node);
-                foreachVariable = (var_definition_node)dn;
-                if (!check_name_in_current_scope(_foreach_stmt.identifier.name))
-                    AddError(loc1, "FOREACH_LOOP_CONTROL_MUST_BE_SIMPLE_LOCAL_VARIABLE");
-            }
-            else
-            {
-                foreachVariable = context.add_var_definition(_foreach_stmt.identifier.name, get_location(_foreach_stmt.identifier));
-
-                type_node tn;
-                if (_foreach_stmt.type_name is SyntaxTree.no_type_foreach) // значит, это for var x in a
-                {
-                    tn = elem_type;
-                }
-                else // значит, это for var x: T in a
-                {
-                    tn = convert_strong(_foreach_stmt.type_name);
-                    check_for_type_allowed(tn, get_location(_foreach_stmt.type_name));
-                }
-
-                context.close_var_definition_list(tn, null);
-            }
-
-            if (!(foreachVariable.type is compiled_generic_instance_type_node) && !sys_coll_ienum)
-                convertion_data_and_alghoritms.check_convert_type_with_inheritance(elem_type, foreachVariable.type, get_location(_foreach_stmt.identifier));
+            var foreachVariable = FindForeachVariable(_foreach_stmt, elem_type, sys_coll_ienum);
 
             statements_list sl = new statements_list(get_location(_foreach_stmt.stmt));
             convertion_data_and_alghoritms.statement_list_stack_push(sl);
@@ -109,6 +82,47 @@ namespace PascalABCCompiler.TreeConverter
 
             return_value(sl2);
         }
+
+
+        
+        private var_definition_node FindForeachVariable(foreach_stmt _foreach_stmt, type_node elem_type, bool sys_coll_ienum)
+        {
+            var_definition_node foreachVariable = null;
+            if (_foreach_stmt.type_name == null) // значит, переменная определена в другом месте
+            {
+                location loc1 = get_location(_foreach_stmt.identifier);
+                definition_node dn = context.check_name_node_type(_foreach_stmt.identifier.name, loc1,
+                    general_node_type.variable_node);
+                foreachVariable = (var_definition_node) dn;
+                if (!check_name_in_current_scope(_foreach_stmt.identifier.name))
+                    AddError(loc1, "FOREACH_LOOP_CONTROL_MUST_BE_SIMPLE_LOCAL_VARIABLE");
+            }
+            else
+            {
+                foreachVariable = context.add_var_definition(_foreach_stmt.identifier.name,
+                    get_location(_foreach_stmt.identifier));
+
+                type_node tn;
+                if (_foreach_stmt.type_name is SyntaxTree.no_type_foreach) // значит, это for var x in a
+                {
+                    tn = elem_type;
+                }
+                else // значит, это for var x: T in a
+                {
+                    tn = convert_strong(_foreach_stmt.type_name);
+                    check_for_type_allowed(tn, get_location(_foreach_stmt.type_name));
+                }
+
+                context.close_var_definition_list(tn, null);
+            }
+
+            if (!(foreachVariable.type is compiled_generic_instance_type_node) && !sys_coll_ienum)
+                convertion_data_and_alghoritms.check_convert_type_with_inheritance(elem_type, foreachVariable.type,
+                    get_location(_foreach_stmt.identifier));
+            return foreachVariable;
+        }
+
+
 
         /// <summary>
         /// Преобразует foreach в for, если коллекция это одномерный массив.
