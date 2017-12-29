737071f6acfd845452d8bbc9456b364ebe150162
miks1965 <miks@math.sfedu.ru>
2017-12-30 01:46:45 +0300

    bug fix #617

--- InstallerSamples/Graph3D/animOperationsPlusMult.pas
+++ InstallerSamples/Graph3D/animOperationsPlusMult.pas
@@ -1,13 +1,13 @@
-uses Graph3D;
+п»їuses Graph3D;
 
 begin
-  Window.Title := 'a+b+c - последовательное выполнение, a*b - параллельное выполнение';
+  Window.Title := 'a+b+c - РїРѕСЃР»РµРґРѕРІР°С‚РµР»СЊРЅРѕРµ РІС‹РїРѕР»РЅРµРЅРёРµ, a*b - РїР°СЂР°Р»Р»РµР»СЊРЅРѕРµ РІС‹РїРѕР»РЅРµРЅРёРµ';
   var s := Box(0,0,0,3,1,2,Colors.Blue);
   var p1 := P3D(5,0,0);
   var p2 := P3D(5,5,0);
   var OrtZ := V3D(0,0,1);
-  var an := s.MoveToAnim(p1,1.Sec)*s.ScaleAnim(1.5,1.Sec) + 
-            s.MoveToAnim(p2,1.Sec)*s.ScaleAnim(1,1.Sec) + 
-            s.RotateAnim(OrtZ,90,1.Sec);
+  var an := s.AnimMoveTo(p1,1.Sec)*s.AnimScale(1.5,1.Sec) + 
+            s.AnimMoveTo(p2,1.Sec)*s.AnimScale(1,1.Sec) + 
+            s.AnimRotate(OrtZ,90,1.Sec);
   an.Begin;
 end.
