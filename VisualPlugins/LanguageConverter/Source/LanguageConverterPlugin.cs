12c97a26ab002d72548c0fe482c6f4cbd4a19e12
Бондарев Иван <ibond84@googlemail.com>
2018-01-01 13:14:03 +0100

    copyright 2005-2018

--- VisualPlugins/LanguageConverter/Source/LanguageConverterPlugin.cs
+++ VisualPlugins/LanguageConverter/Source/LanguageConverterPlugin.cs
@@ -33,7 +33,7 @@ namespace VisualPascalABCPlugins
         {
             get
             {
-                return "Copyright © 2005-2017 by Ivan Bondarev, Stanislav Mihalkovich";
+                return "Copyright © 2005-2018 by Ivan Bondarev, Stanislav Mihalkovich";
             }
         }
 
