3e1d264cbdc11ef48a6f855ff9dbd1c4c3179525
Бондарев Иван <ibond84@googlemail.com>
2018-04-19 20:11:51 +0200

    bug fix #778

---
+++ TestSuite/yield4.pas
@@ -0,0 +1,16 @@
+﻿type
+  t1 = class;
+  
+  t1 = class
+    function f1: sequence of byte;
+    begin
+      yield 0;//обязательно yield
+      yield 1;
+    end;
+  end;
+
+begin 
+  var o1:t1 := new t1;
+  var arr := o1.f1.ToArray;
+  assert(arr[1] = 1);
+end.
