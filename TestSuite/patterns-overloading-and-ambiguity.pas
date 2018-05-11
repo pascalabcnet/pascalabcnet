5bed9fa9958bfef174a978b67f5039c6e4f95002
Alexander Zakharenko <wisestump@yandex.ru>
2018-05-11 21:19:13 +0300

    Change test

--- TestSuite/patterns-overloading-and-ambiguity.pas
+++ TestSuite/patterns-overloading-and-ambiguity.pas
@@ -2,27 +2,37 @@
   My<T> = class
   public 
     _a: T;
-  
-  private 
+    
     procedure Deconstruct(var a: T);
     begin
-      Println('Instance');
       a := _a;
     end;
   end;
 
-procedure Deconstruct<T>(self: My<T>; var a: T);
+procedure Deconstruct<T>(self: My<T>; var a: T); extensionmethod;
 begin
-  Println('Ext');
+  Assert(false);
+  a := self._a;
+end;
+
+procedure Deconstruct<T>(self: My<T>; var a: object); extensionmethod;
+begin
+  a := self._a;
+end;
+
+procedure Deconstruct<T>(self: My<T>; var a: object; var b: object); extensionmethod;
+begin
+  Assert(false);
   a := self._a;
 end;
 
 begin
-  var l := new My<List<integer>>;
+  var l := new My<List<integer>>; 
   l._a := Arr(1, 2, 3).ToList;
   match l with
-    My<real>(a): Assert(false);
-    My<integer>(a): Assert(false);
-    My<List<integer>>(a): Assert(a[0] = 1);
+    My<real>(a: real): Assert(false);
+    My<integer>(a: integer): Assert(false);
+    My<List<integer>>(a: object): ;
+    else Assert(false);
   end;
 end.
