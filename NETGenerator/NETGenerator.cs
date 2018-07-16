5597d0ef2fd5a239ac308aa04fd4b442b3885da5
Бондарев Иван <ibond84@googlemail.com>
2018-07-16 20:41:07 +0200

    bug fix #974

--- NETGenerator/NETGenerator.cs
+++ NETGenerator/NETGenerator.cs
@@ -6919,7 +6919,7 @@ namespace PascalABCCompiler.NETGenerator
             {
             	il.Emit(OpCodes.Box, helper.GetTypeReference(value.obj.conversion_type).tp);
             }
-            else if (value.obj.type.is_value_type && !(value.obj is IAddressedExpressionNode) && !(value.obj is IThisNode) && !(value.obj is ICommonMethodCallNode) && !(value.obj is ICommonConstructorCall))
+            else if (value.obj.type.is_value_type && !(value.obj is IAddressedExpressionNode) && !(value.obj is IThisNode) && !(value.obj is ICommonMethodCallNode) && !(value.obj is ICommonStaticMethodCallNode) && !(value.obj is ICommonConstructorCall) && !(value.obj is ICommonNamespaceFunctionCallNode) && !(value.obj is ICommonNestedInFunctionFunctionCallNode))
             {
                 LocalBuilder lb = il.DeclareLocal(helper.GetTypeReference(value.obj.type).tp);
                 il.Emit(OpCodes.Stloc, lb);
