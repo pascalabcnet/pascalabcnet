20265768458e213bf5cf6c253ffa5bf52ed0d04e
Бондарев Иван <ibond84@googlemail.com>
2018-08-06 12:54:40 +0200

    temp fix #919

---
+++ TestSuite/overloads8.pas
@@ -0,0 +1,19 @@
+﻿type Point = auto class
+  x,y: integer;
+end;
+
+var i := 1;
+
+function RandomPoint: Point;
+begin
+  Result := new Point(i, i);
+  Inc(i);
+end;
+
+var a: array of Point;
+
+begin
+  a := ArrGen(10,i->RandomPoint);
+  assert(a.Min(p->p.x) = 1);
+  assert(a.Max(p->p.x) = 10);
+end.
