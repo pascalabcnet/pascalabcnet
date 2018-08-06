4faef09f6ac7465aaeb30cc260ba9a4a0bcc2b10
Бондарев Иван <ibond84@googlemail.com>
2018-08-06 19:54:35 +0200

    bug fix #982

--- NETGenerator/NETGenerator.cs
+++ NETGenerator/NETGenerator.cs
@@ -3901,9 +3901,9 @@ namespace PascalABCCompiler.NETGenerator
                     else
                     {
                         if (rank > 1)
-                            CreateNDimUnsizedArray(il, (ElementValues[i] as IArrayInitializer).type, helper.GetTypeReference((ElementValues[i] as IArrayInitializer).type.element_type), rank, get_sizes(ElementValues[0] as IArrayInitializer, rank), lb.LocalType.GetElementType());
+                            CreateNDimUnsizedArray(il, (ElementValues[i] as IArrayInitializer).type, helper.GetTypeReference((ElementValues[i] as IArrayInitializer).type.element_type), rank, get_sizes(ElementValues[i] as IArrayInitializer, rank), lb.LocalType.GetElementType());
                         else
-                            CreateUnsizedArray(il, helper.GetTypeReference((ElementValues[i] as IArrayInitializer).type.element_type), (ElementValues[0] as IArrayInitializer).ElementValues.Length, lb.LocalType.GetElementType());
+                            CreateUnsizedArray(il, helper.GetTypeReference((ElementValues[i] as IArrayInitializer).type.element_type), (ElementValues[i] as IArrayInitializer).ElementValues.Length, lb.LocalType.GetElementType());
                         il.Emit(OpCodes.Stelem, ArrType);
                         il.Emit(OpCodes.Ldloc, lb);
                         PushIntConst(il, i);
@@ -3927,7 +3927,7 @@ namespace PascalABCCompiler.NETGenerator
                         PushIntConst(il, i);
                         il.Emit(OpCodes.Ldelema, ti.tp);
                         il.Emit(OpCodes.Stloc, llb);
-                        if (ElementValues[0] is IRecordConstantNode)
+                        if (ElementValues[i] is IRecordConstantNode)
                             GenerateRecordInitCode(il, llb, ElementValues[i] as IRecordConstantNode);
                         else GenerateRecordInitCode(il, llb, ElementValues[i] as IRecordInitializer, true);
                     }
@@ -4023,7 +4023,7 @@ namespace PascalABCCompiler.NETGenerator
                     }
                     else
                     {
-                        CreateUnsizedArray(il, helper.GetTypeReference((ElementValues[i] as IArrayConstantNode).type.element_type), (ElementValues[0] as IArrayConstantNode).ElementValues.Length, lb.LocalType.GetElementType());
+                        CreateUnsizedArray(il, helper.GetTypeReference((ElementValues[i] as IArrayConstantNode).type.element_type), (ElementValues[i] as IArrayConstantNode).ElementValues.Length, lb.LocalType.GetElementType());
                         il.Emit(OpCodes.Stelem, ArrType);
                         il.Emit(OpCodes.Ldloc, lb);
                         PushIntConst(il, i);
---
+++ TestSuite/dynarrays4.pas
@@ -0,0 +1,10 @@
+﻿var c2: array of array of integer := ((1,2,3),(4,5),(6,7,8));
+begin
+  var c: array of array of integer := ((1,2,3),(4,5),(6,7,8));
+  assert(c[0].Length = 3);
+  assert(c[1].Length = 2);
+  assert(c[2].Length = 3);
+  assert(c2[0].Length = 3);
+  assert(c2[1].Length = 2);
+  assert(c2[2].Length = 3);
+end.
