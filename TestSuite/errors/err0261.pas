1efb46a659459029a5c066a8f021bc39f342877d
Бондарев Иван <ibond84@googlemail.com>
2018-09-04 21:02:18 +0200

    fix #1149

---
+++ TestSuite/errors/err0261.pas
@@ -0,0 +1,11 @@
+﻿type
+  t1<T>=class
+  
+    private class function MessageBox(wnd: System.IntPtr; message, caption: string; flags: cardinal):integer; 
+    external 'User32.dll';
+    
+  end;
+
+begin
+  var a := new t1<byte>;
+end.
