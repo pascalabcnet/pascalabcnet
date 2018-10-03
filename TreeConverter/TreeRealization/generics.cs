37733573a5965f97ffeecf8fbf4a4e3d54b32b0d
Бондарев Иван <ibond84@googlemail.com>
2018-10-03 21:21:24 +0200

    fix #1265

--- TreeConverter/TreeRealization/generics.cs
+++ TreeConverter/TreeRealization/generics.cs
@@ -1334,6 +1334,8 @@ namespace PascalABCCompiler.TreeRealization
 
         public static bool type_has_default_ctor(type_node tn, bool find_protected_ctors)
         {
+            if (tn.is_generic_parameter && tn.base_type != null && tn.base_type.IsAbstract)
+                return false;
             List<SymbolInfo> sil = tn.find_in_type(compiler_string_consts.default_constructor_name, tn.Scope);
             if (sil != null)
             {
