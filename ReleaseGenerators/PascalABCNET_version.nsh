9aad3c7d3f1d3d6a36dca4ecc91514b57c0cc8f6
miks1965 <miks@math.sfedu.ru>
2018-10-07 13:55:36 +0300

    Исправлена справка по exit, break

--- ReleaseGenerators/PascalABCNET_version.nsh
+++ ReleaseGenerators/PascalABCNET_version.nsh
@@ -1,1 +1,1 @@
-!define VERSION '3.4.2.1822'
+!define VERSION '3.4.2.1825'
---
+++ TestSuite/typed_var_init1.pas
@@ -0,0 +1,7 @@
+﻿var c: string := '123'[1:3];
+var c1: string := '123'[1];
+
+begin
+  Assert(c='12');
+  Assert(c1='1')
+end.
