86f66b163b50b7431b21420866116787565c7263
miks1965 <miks@math.sfedu.ru>
2018-11-18 18:31:03 +0300

    !GenDocGraph3D

---
+++ PABCNetHelp/DocGenForStandardUnits/!GenDocGraph3D.pas
@@ -0,0 +1,12 @@
+﻿uses GenDocUnit;
+
+begin
+  fname := '..\..\bin\Lib\Graph3D.pas';
+  RootOutputDirectory := 'PABCUnits\Graph3D\';
+  Step1;
+  Step2;
+  Step3;
+  Step3_1;
+  Step4;
+  FinalStep;
+end.
