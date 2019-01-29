eddc0ad060e280e1bb8c44b99f6bb9898a370614
Бондарев Иван <ibond84@googlemail.com>
2019-01-29 21:29:43 +0100

    fix #1659

--- CodeCompletion/CodeFormatter.cs
+++ CodeCompletion/CodeFormatter.cs
@@ -581,9 +581,9 @@ namespace CodeFormatters
                     else if (comm.StartsWith("var") && comm.EndsWith("("))
                         comm = RemoveOverSpaces(comm);
                     else if (trimed_comm.StartsWith("[") && trimed_comm.EndsWith("]"))
-                    {
                         comm = RemoveOverSpaces(comm).Replace("[ ","[").Replace(" ]", "]");
-                    }
+                    else if ((comm.StartsWith("auto") || comm.StartsWith("sealed") || comm.StartsWith("abstract") || comm.StartsWith("static")) && (trimed_comm.EndsWith("class") || trimed_comm.EndsWith("record")))
+                        comm = RemoveOverSpaces(comm);
                 }
                 WriteCommentWithIndent(comm, true);
                 read_from_beg_pos = false;
