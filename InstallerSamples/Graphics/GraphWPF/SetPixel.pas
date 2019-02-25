617428ee932693a9210b7bde5d214d7265c8d5c8
Mikhalkovich Stanislav <miks@math.sfedu.ru>
2019-02-25 12:03:43 +0300

    Новый GraphWPG с более плавной и быстрой графикой, SetPixel, SetPixels

---
+++ InstallerSamples/Graphics/GraphWPF/SetPixel.pas
@@ -0,0 +1,9 @@
+﻿uses GraphWPF;
+
+begin
+  Window.SetSize(640, 480);
+  for var x := 0 to Window.Width.Round do
+  for var y := 0 to Window.Height.Round do
+    SetPixel(x, y, RGB(2 * x - y, x - 3 * y, x + y));
+  Milliseconds.Println;  
+end.
