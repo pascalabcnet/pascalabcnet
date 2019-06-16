f1fce6b524ecf08d3a2a638461743a5603f61a0e
Mikhalkovich Stanislav <miks@math.sfedu.ru>
2019-06-16 10:30:06 +0300

    Продолжение предыдущего коммита

--- NETGenerator/NETGenerator.cs
+++ NETGenerator/NETGenerator.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Ivan Bondarev, Stanislav Mihalkovich (for details please see \doc\copyright.txt)
 // This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
 using System;
+using System.Linq;
 using PascalABCCompiler.SemanticTree;
 using System.Threading;
 using System.Reflection;
