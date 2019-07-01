53ea4a7c3ebff301962b4b692157d1ad4a2a6674
Mikhalkovich Stanislav <miks@math.sfedu.ru>
2019-07-01 22:46:03 +0300

    fix #1894

--- ReleaseGenerators/PascalABCNET_version.nsh
+++ ReleaseGenerators/PascalABCNET_version.nsh
@@ -1,1 +1,1 @@
-!define VERSION '3.5.0.2170'
+!define VERSION '3.5.0.2171'
---
+++ TestSuite/xor_byte_int_fun.pas
@@ -0,0 +1,7 @@
+﻿function f1: integer := 256;
+
+begin
+  var b: byte := 255; 
+  var i := f1 xor b; 
+  Assert(i=511);
+end.
