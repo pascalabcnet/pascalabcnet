e6d19d0d5da459c7d1ef19400fba8fea9ae1a890
Mikhalkovich Stanislav <miks@math.sfedu.ru>
2019-07-25 13:16:10 +0300

    Вернул назад неверное исправление if then else

--- Configuration/GlobalAssemblyInfo.cs
+++ Configuration/GlobalAssemblyInfo.cs
@@ -15,7 +15,7 @@ internal static class RevisionClass
 	public const string Major = "3";
 	public const string Minor = "5";
 	public const string Build = "0";
-	public const string Revision = "2205";
+	public const string Revision = "2206";
 
         public const string MainVersion = Major + "." + Minor;
 	public const string FullVersion = Major + "." + Minor + "." + Build + "." + Revision;
