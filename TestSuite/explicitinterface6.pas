c533fc4dd29645af87f99e2881ae8cd7e2001313
Бондарев Иван <ibond84@googlemail.com>
2020-08-16 12:25:28 +0200

    bug fix #2259

---
+++ TestSuite/explicitinterface6.pas
@@ -0,0 +1,30 @@
+﻿var a: integer;
+
+type
+  I1 = interface
+    
+    procedure p1(i: integer);
+    procedure p1(s: string);
+    
+  end;
+  
+  t1 = class(I1)
+    
+    public procedure I1.p1(i: integer);
+    begin
+      a := 1;
+    end;
+    public procedure I1.p1(s: string);
+    begin
+      a := 2;
+    end;
+    
+  end;
+  
+begin 
+  var o: I1 := new t1;
+  o.p1(2);
+  assert(a = 1);
+  o.p1('');
+  assert(a = 2);
+end.
