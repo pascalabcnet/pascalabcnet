db107c5ebfd984c8b3d2a5f3b7cae5b71bb664ed
Mikhalkovich Stanislav <miks@math.sfedu.ru>
2020-09-19 23:45:19 +0300

    Здоровье кода в Status Bar

--- Compiler/ICompiler.cs
+++ Compiler/ICompiler.cs
@@ -14,6 +14,13 @@ namespace PascalABCCompiler
     public enum CompilerType { Standart, Remote }
     public interface ICompiler
     {
+        /// Здоровье кода на всякий случай выносим в интерфейс компилятора
+        /// Реально оно будет использоваться только при запуске из под оболочки (Remote Compiler)
+        int PABCCodeHealth  
+        {
+            get;
+        }
+
         SyntaxTreeConvertersController SyntaxTreeConvertersController
         {
             get; 
