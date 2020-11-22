899f0da1bdb6b42828548209bf42fb6aed092798
Бондарев Иван <ibond84@googlemail.com>
2020-11-22 17:10:21 +0100

    bug fix #2342

---
+++ TestSuite/units/u_lambda_any.pas
@@ -0,0 +1,14 @@
+﻿unit u_lambda_any;
+{$reference WindowsBase.dll}
+
+function f: integer;
+begin
+  var d := System.Windows.Threading.Dispatcher.CurrentDispatcher;
+  var res := d.Invoke(()->
+  begin
+    Result := 1;
+  end);
+  Result := res;
+end;
+
+end.
