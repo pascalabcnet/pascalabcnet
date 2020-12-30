73609701177ee67e40f7a339b66e9df31fb5af2e
Ivan Bondarev <ibond84@googlemail.com>
2020-12-30 14:54:53 +0100

    bug fix #2375

--- TreeConverter/TreeConversion/syntax_tree_visitor.cs
+++ TreeConverter/TreeConversion/syntax_tree_visitor.cs
@@ -3918,6 +3918,7 @@ namespace PascalABCCompiler.TreeConverter
                     record_type_name = null;
                     record_is_generic = false;
                     context.converted_type.internal_is_value = true;
+
                     if (_class_definition.body == null &&
                         (_class_definition.class_parents == null || _class_definition.class_parents.types.Count == 0))
                     {
@@ -3930,6 +3931,7 @@ namespace PascalABCCompiler.TreeConverter
                     }
                     else
                     {
+                        
                         context.converted_type.ForwardDeclarationOnly = false;
                         weak_node_test_and_visit(_class_definition.body);
                         
@@ -4051,7 +4053,13 @@ namespace PascalABCCompiler.TreeConverter
         
         private void visit_class_member_realizations(SyntaxTree.class_body_list _class_body)
         {
-        	foreach (SyntaxTree.class_members clmem in _class_body.class_def_blocks)
+            if (context.converted_type.internal_is_value)
+            {
+                context.add_equal_operator_if_need();
+                context.add_notequal_operator_if_need();
+            }
+            
+            foreach (SyntaxTree.class_members clmem in _class_body.class_def_blocks)
             {
         		foreach (SyntaxTree.declaration sd in clmem.members)
             	{
