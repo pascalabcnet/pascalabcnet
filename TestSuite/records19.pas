cd1585fead29e781cfaa49126f6dda80705f94b0
Ivan Bondarev <ibond84@googlemail.com>
2021-01-05 10:45:39 +0100

    #2377 sample 1

---
+++ TestSuite/records19.pas
@@ -0,0 +1,21 @@
+﻿var i: integer;
+
+type
+  r1 = record 
+    public function InitI1:integer;
+    begin
+      Result := 1;
+      Inc(i);
+    end;
+    
+    public i1: integer := InitI1;
+    public i2: integer;
+    
+  end;
+  
+function f1 := default(r1);
+
+begin
+  var a := f1;
+  assert(i = 1);
+end.
