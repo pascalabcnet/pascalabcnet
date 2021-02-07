5447943fefec241f46bb2fd1172eff94dc22a9c6
Sun Serega <latchenko3@yandex.ru>
2021-02-07 12:40:25 +0200

    cleanup

---
+++ TestSuite/units/u_partial_2432.pas
@@ -0,0 +1,18 @@
+﻿unit u_partial_2432;
+
+interface
+
+type
+  t0<T> = partial class end;
+  t1<T> = partial class(t0<T>)
+    
+  end;
+  
+implementation
+
+type
+  t1<T> = partial class(t0<T>)
+    
+  end;
+  
+end.
---
+++ TestSuite/usesunits/use_partial_2432.pas
@@ -0,0 +1,6 @@
+﻿uses u_partial_2432;
+
+begin
+  new t0<byte>;
+  new t1<word>;
+end.
