903e6ed63c6b29bb32327effb4da2d2e876f2901
Ivan Bondarev <ibond84@googlemail.com>
2021-02-21 12:33:51 +0100

    #2436

---
+++ TestSuite/nil7.pas
@@ -0,0 +1,18 @@
+﻿type
+  t0 = class end;
+  
+  t1 = class
+    static function operator implicit(p: t0): t1 := new t1;
+  end;
+
+var obj: t1;
+  
+procedure p1(o: t1);
+begin
+  obj := o;
+end;
+
+begin
+  p1(t0(nil));
+  assert(obj <> nil);
+end.
