1b611fd47fe9c4023f76073bb2ca06a4f45d1050
Ivan Bondarev <ibond84@googlemail.com>
2021-04-11 11:54:18 +0200

    #2469

---
+++ TestSuite/errors/err0412.pas
@@ -0,0 +1,8 @@
+﻿type
+  t1 = abstract class(System.Collections.IEnumerator) 
+    // Падает только если НЕ указать System.Collections
+    public property System.Collections<integer>.IEnumerator.Current: object read nil;
+    
+  end;
+  
+begin end.
