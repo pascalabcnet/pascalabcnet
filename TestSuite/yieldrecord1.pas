3c2f2ee5ef2a1035fffcc0b7e7ec20b290d4d5fc
Ivan Bondarev <ibond84@googlemail.com>
2021-05-02 12:19:45 +0200

    #2479

---
+++ TestSuite/yieldrecord1.pas
@@ -0,0 +1,20 @@
+﻿type
+  t1 = record 
+    i: integer :=1;
+  end;
+  
+function f1: sequence of t1;
+begin
+  var o: t1;
+  yield o;
+end;
+
+begin
+  var i := 0; 
+  foreach var it in f1 do
+  begin
+    assert(it.i = 1);
+    Inc(i);
+  end;
+  assert(i = 1);
+end.
