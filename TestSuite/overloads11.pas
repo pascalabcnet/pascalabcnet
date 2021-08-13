651d41d3901bec6cc0f8fb737b21d043a6118cfc
Ivan Bondarev <ibond84@googlemail.com>
2021-08-13 17:20:48 +0200

    overloads11.pas deleted

--- TestSuite/overloads11.pas
+++
@@ -1,3 +0,0 @@
-﻿begin
-  System.Globalization.UnicodeCategory.Format{@static function Enum.Format(enumType: Type; value: Object; format: string): string;@}(typeof(System.Globalization.UnicodeCategory), a, 'd');
-end.
