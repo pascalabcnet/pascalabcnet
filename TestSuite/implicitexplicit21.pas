62f031f0d022ff3659b05d881f32293b3887e051
Ivan Bondarev <ibond84@googlemail.com>
2021-10-31 11:44:21 +0100

    #2553

---
+++ TestSuite/implicitexplicit21.pas
@@ -0,0 +1,20 @@
+﻿var i: integer;
+type
+  Generic1<T> = class end;
+  Generic2<T1,T2> = class end;
+  
+  ConvTarget = class
+    
+    static function operator implicit<T1, T2>(o: Generic2<T1, Generic1<T2>>): ConvTarget; where T1, T2: record;
+    begin
+      Result := new ConvTarget;
+      i := 1;
+    end;
+    
+  end;
+  
+begin
+  var a: Generic2<byte, Generic1<word>>;
+  var o: ConvTarget := a;
+  assert(i = 1);
+end.
