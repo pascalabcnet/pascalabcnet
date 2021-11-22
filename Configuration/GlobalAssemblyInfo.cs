e758c710ba1a95044cd91658f1b27cf18f9c9861
Mikhalkovich Stanislav <miks@math.sfedu.ru>
2021-11-22 22:18:28 +0300

    Пересобрал версию без ложного срабатывания AVG

--- Configuration/GlobalAssemblyInfo.cs
+++ Configuration/GlobalAssemblyInfo.cs
@@ -15,7 +15,7 @@ internal static class RevisionClass
 	public const string Major = "3";
 	public const string Minor = "8";
 	public const string Build = "2";
-	public const string Revision = "3032";
+	public const string Revision = "3033";
 
         public const string MainVersion = Major + "." + Minor;
 	public const string FullVersion = Major + "." + Minor + "." + Build + "." + Revision;
--- ReleaseGenerators/PascalABCNET_version.nsh
+++ ReleaseGenerators/PascalABCNET_version.nsh
@@ -1,1 +1,1 @@
-!define VERSION '3.8.2.3032'
+!define VERSION '3.8.2.3033'
