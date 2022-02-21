6381b157afc4b8bb4c5f2e8aa95f032c585f5420
Mikhalkovich Stanislav <miks@math.sfedu.ru>
2022-02-21 17:40:32 +0300

    Пример и тест for step

---
+++ InstallerSamples/WhatsNew/3_8_3/for_step.pas
@@ -0,0 +1,15 @@
+﻿type Days = (Mon,Tue,Wed,Thi,Fri,sat);
+
+begin
+  for var i:=1 to 5 step 2 do
+    Print(i);
+  Println;
+  
+  for var i:='5' to '2' step -2 do
+    Print(i);
+  Println;
+  
+  for var i:=Mon to Sat step 2 do
+    Print(i);
+  Println;
+end.
---
+++ TestSuite/for_step1.pas
@@ -0,0 +1,18 @@
+﻿type Days = (Mon,Tue,Wed,Thi,Fri,sat);
+
+begin
+  var l := new List<integer>;
+  for var i:=1 to 5 step 2 do
+    l.Add(i);
+  Assert(l.SequenceEqual(Arr(1,3,5)));
+  
+  var lc := new List<char>;
+  for var i:='5' to '2' step -2 do
+    lc.Add(i);
+  Assert(lc.SequenceEqual(Arr('5','3')));
+  
+  var ld := new List<Days>;
+  for var i:=Mon to Sat step 2 do
+    ld.Add(i);
+  Assert(ld.SequenceEqual(Arr(Mon,Wed,Fri)));
+end.
