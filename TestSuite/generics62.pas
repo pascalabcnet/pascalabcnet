43f21dc033ce46d88d1df8c2696a5cfeb56f9f8b
Ivan Bondarev <ibond84@googlemail.com>
2022-04-03 14:08:36 +0200

    test for #2644

---
+++ TestSuite/generics62.pas
@@ -0,0 +1,12 @@
+﻿var i: integer;
+procedure p1<T1>(d: T1->()) := exit;
+procedure p1<T1,T2>(d: T1->T2);
+begin
+  i := 1;
+end;
+
+function f(b: byte) := 0;
+begin
+  p1(f);
+  assert(i = 1);
+end.
