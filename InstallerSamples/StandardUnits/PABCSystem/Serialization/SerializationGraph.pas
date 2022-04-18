69e057c576d377f97d8fd6387e6547f4a60bcfe1
Mikhalkovich Stanislav <miks@math.sfedu.ru>
2022-04-18 21:34:18 +0300

    Добавлены примеры в Whats New

--- InstallerSamples/StandardUnits/PABCSystem/Serialization/SerializationGraph.pas
+++ InstallerSamples/StandardUnits/PABCSystem/Serialization/SerializationGraph.pas
@@ -12,7 +12,7 @@ type
 const fname = 'a.dat';
 
 begin
-  var m := new Node(5,new Node(3,new Node(4,nil)));
+  var m := new Node(5,new Node(3,new Node(4,nil))); // Связный  список
   Serialize(fname,m); // Сериализуем объект в файл
   var m1 := Deserialize(fname) as Node; // Десериализуем из файла
   Print(m1);
