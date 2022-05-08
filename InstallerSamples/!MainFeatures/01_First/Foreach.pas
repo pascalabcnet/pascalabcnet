104f0d43dad2ba5c054dbc7340e8d84eefad6868
Mikhalkovich Stanislav <miks@math.sfedu.ru>
2022-05-08 19:37:15 +0300

    [cache] для одного параметра не оборачивает его в Tuple - за счет этого ускорена в 10 раз

--- InstallerSamples/!MainFeatures/01_First/Foreach.pas
+++ InstallerSamples/!MainFeatures/01_First/Foreach.pas
@@ -1,24 +1,30 @@
-// Пример иллюстрирует возможности оператора foreach
+﻿// Пример иллюстрирует возможности оператора foreach
 var 
   a: array [1..5] of integer := (1,3,5,7,9);
   s: set of integer;
   l: List<integer>;
 
 begin
-  write('foreach по обычному массиву: ':35);
+  Write('foreach по обычному массиву: ':35);
   foreach x: integer in a do
     Print(x);
-  writeln;  
+  Println;  
   
   s := [2..5,10..14];
-  write('foreach по множеству: ':35);
+  Write('foreach по множеству: ':35);
   foreach var x in s do
     Print(x);
-  writeln;  
+  Println;  
   
   l := new List<integer>;
   l.Add(7); l.Add(2); l.Add(5);
-  write('foreach по динамическому массиву: ':35);
+  Write('foreach по динамическому массиву: ':35);
   foreach var x in l do
     Print(x);
+  Println;  
+
+  Write('foreach по диапазону: ':35);
+  foreach var x in 1..10 do
+    Print(x);
+  Println;  
 end.
--- ReleaseGenerators/PascalABCNET_version.nsh
+++ ReleaseGenerators/PascalABCNET_version.nsh
@@ -1,1 +1,1 @@
-!define VERSION '3.8.3.3115'
+!define VERSION '3.8.3.3116'
