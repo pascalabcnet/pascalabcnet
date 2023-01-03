7f73d91024084b60c5b1cdebd0b3fa53171cb932
Mikhalkovich Stanislav <miks@math.sfedu.ru>
2023-01-03 12:47:46 +0300

    ML example bad

---
+++ W/Debug_Folder/a1.pas
@@ -0,0 +1,11 @@
+﻿{$reference Microsoft.ML.Core.dll}
+{$reference Microsoft.ML.Data.dll}
+{$reference Microsoft.ML.KMeansClustering.dll}
+
+uses Microsoft.ML;
+uses Microsoft.ML.Data;
+uses Microsoft.ML.Trainers;
+ 
+begin
+  var trainer: IEstimator<ClusteringPredictionTransformer<KMeansModelParameters>>;
+end.
