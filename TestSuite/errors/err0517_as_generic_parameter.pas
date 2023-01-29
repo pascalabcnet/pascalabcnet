23635ace4007647cc53f63d237a4c22a48981621
Ivan Bondarev <ibond84@googlemail.com>
2023-01-29 12:25:01 +0100

    #2795

---
+++ TestSuite/errors/err0517_as_generic_parameter.pas
@@ -0,0 +1,9 @@
+﻿//!Операция as не может быть применена к обобщённому параметру T, не имеющему спецификатора "class"
+type
+  A<T> = class
+    public static procedure Test(x: T) := Print(x as T);
+  end;
+
+begin
+  A&<integer>.Test(2); // выводит мусор
+end.
