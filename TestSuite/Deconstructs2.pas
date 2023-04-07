052d0e38a948680ece6c99885eba9c7dbf176bd9
Ivan Bondarev <ibond84@googlemail.com>
2023-04-07 14:09:53 +0200

    #2833

---
+++ TestSuite/Deconstructs2.pas
@@ -0,0 +1,16 @@
+﻿var i: integer;
+type
+  t1 = class
+    procedure Deconstruct;
+    begin
+      i := 1;
+    end;
+  end;
+  
+begin
+  if new t1 is t1(var a) then
+    begin
+      i := 2;
+    end;
+  assert(i = 2);
+end.
