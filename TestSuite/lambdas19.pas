fb5ecd37803d4594ea726b60560986eec391c5b6
Ivan Bondarev <ibond84@googlemail.com>
2023-11-01 21:51:05 +0100

    test for #2686

---
+++ TestSuite/lambdas19.pas
@@ -0,0 +1,19 @@
+﻿var i: integer;
+
+procedure p1<T>(p: procedure);
+begin
+  var pp := procedure->
+  begin
+    // Обязательно захватить p
+    p := p;
+    Inc(i);
+  end;
+  pp;
+end;
+
+begin 
+  p1&<integer>(()->begin
+                     
+                   end);
+  assert(i = 1);
+end.
