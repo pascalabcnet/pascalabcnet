6bc05f0f9073f5e60288689e954e1a70cdf1f543
Ivan Bondarev <ibond84@googlemail.com>
2023-11-19 11:53:57 +0100

    #2957

---
+++ TestSuite/errors/err0529.pas
@@ -0,0 +1,21 @@
+﻿//!Несколько подпрограмм proc1 могут быть вызваны
+type
+  c1 = class
+    // обязательно метод. с подпрограммой не воспроизводится
+    procedure method1(act: System.Action);
+    begin end;
+  end;
+  
+procedure proc1(par1: System.Action<integer>); begin end;
+procedure proc1(par1: System.Action<real>); begin end;
+
+begin
+  var var1:= new c1;
+  
+  var1.method1( 
+      ()-> begin
+          proc1(par1-> begin end);
+      end
+  );
+  
+end.
