79a97b72b9e4df9d0716ef6467d1f4ae28a6cfdf
nevermind322 <81963698+nevermind322@users.noreply.github.com>
2023-12-25 20:17:20 +0300

    enable VarNamesInMethodsWithSameNameAsClassGenericParamsReplacer

--- SyntaxTreeConverters/StandardSyntaxConverter.cs
+++ SyntaxTreeConverters/StandardSyntaxConverter.cs
@@ -37,7 +37,7 @@ namespace PascalABCCompiler.SyntaxTreeConverters
 
             // Выносим выражения с лямбдами из заголовка foreach + считаем максимум 10 вложенных лямбд
             StandOutExprWithLambdaInForeachSequenceAndNestedLambdasVisitor.New.ProcessNode(root);
-            //new VarNamesInMethodsWithSameNameAsClassGenericParamsReplacer(root as program_module, getScopeForUnit, addScope).ProcessNode(root); // SSM bug fix #1147
+            new VarNamesInMethodsWithSameNameAsClassGenericParamsReplacer(root as program_module).ProcessNode(root); // SSM bug fix #1147
             FindOnExceptVarsAndApplyRenameVisitor.New.ProcessNode(root);
 
             // loop
