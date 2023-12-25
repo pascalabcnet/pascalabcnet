7622ac608f55f4acf6693451b6bea12f2ff18689
nevermind322 <81963698+nevermind322@users.noreply.github.com>
2023-12-25 14:21:49 +0300

    add rebuildDebug and build units

---
+++ _RebuildDebugAndBuildUnits.bat
@@ -0,0 +1,33 @@
+call Studio.bat /t:rebuild "/property:Configuration=Debug" "/p:Platform=Any CPU" PascalABCNET.sln
+
+@IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+cd ReleaseGenerators
+..\bin\pabcnetc RebuildStandartModules.pas /rebuild
+@IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+cd PABCRtl
+..\..\bin\pabcnetc PABCRtl.pas /rebuild
+@IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+..\sn.exe -Vr PABCRtl.dll
+..\sn.exe -R PABCRtl.dll KeyPair.snk
+..\sn.exe -Vu PABCRtl.dll
+copy PABCRtl.dll ..\..\bin\Lib
+
+cd ..
+ExecHide.exe gacutil.exe /u PABCRtl
+ExecHide.exe gacutil.exe /i ..\bin\Lib\PABCRtl.dll
+
+..\bin\pabcnetc RebuildStandartModules.pas /rebuild
+@IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+
+cd ..\bin
+REM MPGORunner.exe
+cd ..
+GOTO EXIT
+
+:ERROR
+PAUSE
+
+:EXIT
