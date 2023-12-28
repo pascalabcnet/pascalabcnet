9fd8c001ce960dbce1f40a61b54154e580c08a9c
Ivan Bondarev <ibond84@googlemail.com>
2023-12-28 14:17:25 +0100

    #2184

---
+++ TestSuite/abstract5.pas
@@ -0,0 +1,13 @@
+﻿{$reference PresentationFramework.dll}
+{$apptype windows}
+uses System.Windows.Controls;
+
+type
+  //Ошибка: Класс MyTextBox абстрактный и не может иметь атрибут sealed, потому что метод CreateRenderScope не реализован
+  MyTextBox = sealed class(RichTextBox)
+    
+  end;
+  
+begin 
+  new MyTextBox();
+end.
---
+++ TestSuite/usesunits/use_abstract3.pas
@@ -0,0 +1,11 @@
+﻿uses u_abstract3;
+
+type
+  
+  // обязательно t3 - тип из другого модуля
+  //Ошибка: Абстрактный класс не может иметь атрибут sealed
+  t4 = sealed class(t3) end;
+  
+begin
+  new t4;
+end.
