0e3a3c232ee699231d23604f711f7af11ad1d029
Ivan Bondarev <ibond84@googlemail.com>
2023-12-28 11:24:30 +0100

    fix #2736

---
+++ TestSuite/where22.pas
@@ -0,0 +1,22 @@
+﻿type
+  t1<TSelf> = interface
+    where TSelf: t1<TSelf>;
+    function f: integer;
+  end;
+  //Ошибка: Невозможно инстанцировать, так как тип TSelf не реализует интерфейс t1<TSelf>
+  t2<TSelf> = interface(t1<TSelf>)
+    where TSelf: t2<TSelf>;
+    // А если так то работает:
+//    where TSelf: t1<TSelf>, t2<TSelf>;
+  end;
+  
+  TClass = class(t2<TClass>)
+    public function f: integer;
+    begin
+      Result := 1;
+    end;
+  end;
+begin 
+  var obj := new TClass;
+  assert(obj.f = 1);
+end.
