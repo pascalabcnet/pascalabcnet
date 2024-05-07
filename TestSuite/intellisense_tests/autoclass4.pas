bff1425ede5b2d63229741420c0b11d0a057591a
Ivan Bondarev <ibond84@googlemail.com>
2024-05-07 22:04:03 +0200

    fixed semantic intellisense

--- TestSuite/intellisense_tests/autoclass4.pas
+++ TestSuite/intellisense_tests/autoclass4.pas
@@ -3,4 +3,3 @@ var a{@var a: sequence of class;@} := (1..18).Sel(i->new class(f := i, g := i.To
 var x{@var x: string;@} := a.First.g;
 var y{@var y: class;@} := a.First;
 writeln(a.First.g{@property class.g: string;@});
-
