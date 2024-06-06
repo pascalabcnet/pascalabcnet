50d45cda57cfc5af0eab0a938ba44c3529a8f2a6
nevermind322 <81963698+nevermind322@users.noreply.github.com>
2024-06-06 20:57:43 +0300

    remove comments

--- SyntaxVisitors/SugarVisitors/AssignTuplesDesugarVisitor/NewAssignTuplesDesugarVisitor.cs
+++ SyntaxVisitors/SugarVisitors/AssignTuplesDesugarVisitor/NewAssignTuplesDesugarVisitor.cs
@@ -20,22 +20,14 @@ namespace SyntaxVisitors.SugarVisitors
         {
             this.binder = binder; 
         }
+     
 
-        
-        // (a[i], a[j], a[k]) = (a[j], a[k], a[i])
-        //temp = a[i]
-        //temp = a[j]
-        //a[j] = a[k]
-        //a[k] = temp
-        //a[i] = temp
         List<statement> desugar(tuple_node tn, addressed_value_list vars)
         {
-
             var order = Assign.getAssignOrder(tn, vars, binder);
             var assigns = new List<statement>();
             foreach (var a in order)
             {
-                //parents?
                 if (a.to is TempSymbol ts)
                 {
                     var cur = new var_def_statement(ts.node as ident, a.from.node, tn.Parent.source_context);
@@ -50,16 +42,9 @@ namespace SyntaxVisitors.SugarVisitors
             return assigns;
         }
 
-        //a[i]
-        //(a[i], a[j]) = (a[j], a[i])
-        //(a, a[i]) = (b, b[j])
-        //indexer, dot_node
-        //get_address, roof_dereference
+
         public override void visit(assign_tuple node)
         {
-
-            
-
             if (node.expr is tuple_node tn)
             {
                 var n = node.vars.variables.Count();
