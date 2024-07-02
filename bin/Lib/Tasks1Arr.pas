b471da4499359b620715650abf57b3492e0e1849
Mikhalkovich Stanislav <miks@math.sfedu.ru>
2024-07-02 17:06:45 +0300

    Поправлены Tasks1Arr.pas Tasks1Begin.pas Tasks1BoolIfCase.pas

--- bin/Lib/Tasks1Arr.pas
+++ bin/Lib/Tasks1Arr.pas
@@ -6,50 +6,43 @@ var AllTaskNames: array of string;
 
 procedure CheckTaskT(name: string);
 begin
-  FlattenOutput; 
   ClearOutputListFromSpaces;
   case name of
   'Arr0': begin 
-    ClearOutputListFromSpaces;
     CheckData(Empty);
-    CheckOutputSeq(Arr(5,4,3,2,1,777,5,4,3,2,1,777));
+    CheckOutput(5,4,3,2,1,777,5,4,3,2,1,777);
   end;
   'Arr1': begin 
-    ClearOutputListFromSpaces;
     CheckData(Empty);
-    CheckOutputSeq(Arr(777,1,1,1,777));
+    CheckOutput(777,1,1,1,777);
   end;
   'Arr2': begin 
     var n := 5;
-    ClearOutputListFromSpaces;
-    CheckData(Input := |cInt|*n);
+    CheckData(Input := cInt*n);
     GenerateTests(10,tInt*n);
     var il := IntArr(n);
-    CheckOutputSeq(il.Select(x->x+1));
+    CheckOutput(il.Select(x->x+1));
   end;
   'Arr3': begin 
     var n := 5;
-    ClearOutputListFromSpaces;
     CheckData(Input := |cInt|*n);
     GenerateTests(10,tInt*n);
     var il := IntArr(n);
-    CheckOutputSeq(il.Select(x->x*2));
+    CheckOutput(il.Select(x->x*2));
   end;
   'Arr4': begin 
     var n := 10;
-    ClearOutputListFromSpaces;
-    CheckData(Input := |cInt|*n);
+    CheckData(Input := cInt*n);
     GenerateTests(10,tInt*n);
     var il := IntArr(n);
-    CheckOutputSeq(il+il.Select(x->x*x));
+    CheckOutput(il+il.Select(x->x*x));
   end;
   'Arr5': begin 
     var n := 10;
-    ClearOutputListFromSpaces;
-    CheckData(Input := |cInt|*n);
-    GenerateTests(10,tInt*n);
+    CheckData(Input := cInt*n);
+    GenerateTests(10, tInt*n);
     var il := IntArr(n);
-    CheckOutputSeq(il+il.Reverse);
+    CheckOutput(il, il.Reverse);
   end;
   'ArrErr1': begin 
     TaskResult := TaskStatus.ErrFix
@@ -59,88 +52,79 @@ begin
   end;
   'Arr1Sum': begin 
     var n := 10;
-    ClearOutputListFromSpaces;
-    CheckData(Input := |cInt|*n);
-    GenerateTests(10,tInt*n);
+    CheckData(Input := cInt*n);
+    GenerateTests(10, tInt*n);
     var il := InputListAsIntegers.Select(x->x*0.1);
-    CheckOutputSeq(il+il.Sum);
+    CheckOutput(il, il.Sum);
   end;
   'Arr2Prod': begin 
-    CancelMessagesIfInitial := False;
-    ClearOutputListFromSpaces;
+    CancelMessagesIfInitial := False; // !
     var n := 8;
-    CheckData(|cRe|*n, |cRe|*n);
-    GenerateTests(10,tRe(1,10,1)*n);
+    CheckData(cRe*n, cRe*n);
+    GenerateTests(10, tRe(1,10,1)*n);
     var il := ReArr(n).Select(x->x);
     CheckOutputAfterInitial(il.Product);
   end;
   'Arr1_If': begin 
     CancelMessagesIfInitial := False;
-    ClearOutputListFromSpaces;
     var n := 10;
-    CheckData(|cInt|*n, |cInt|*n);
-    GenerateTests(10,tInt*n);
+    CheckData(cInt*n, cInt*n);
+    GenerateTests(10, tInt*n);
     var il := IntArr(n);
-    CheckOutputSeq(il+il.Where(x->x.IsEven));
+    CheckOutput(il, il.Where(x->x.IsEven));
   end;
   'Arr2_If': begin 
     CancelMessagesIfInitial := False;
-    ClearOutputListFromSpaces;
     var n := 10;
-    CheckData(|cInt|*n, |cInt|*n);
+    CheckData(cInt*n, cInt*n);
     GenerateTests(10,tInt*n);
     var il := IntArr(n);
-    CheckOutputSeq(il+il.Where((x,i)->i.IsOdd));
+    CheckOutput(il, il.Where((x,i)->i.IsOdd));
   end;
   'Arr0_Count': begin 
     CancelMessagesIfInitial := False;
-    ClearOutputListFromSpaces;
     var n := 10;
     CheckData(|cInt|*n, |cInt|*n);
     GenerateTests(10,tInt*n);
     var il := IntArr(n);
-    CheckOutputSeq(il+il.Count(x->x.IsOdd));
+    CheckOutput(il, il.Count(x->x.IsOdd));
   end;
   'Arr1_Sum': begin 
-    ClearOutputListFromSpaces;
     var n := InputList.Count;
-    GenerateTests(10,tInt*n);
-    var il := InputListAsIntegers.Where(x->x in 3..5).ToArray;
-    CheckOutputSeq(InputListAsIntegers+il+il.sum);
+    GenerateTests(10, tInt*n);
+    var a := IntArr(n);
+    var il := a.Where(x->x in 3..5).ToArray;
+    CheckOutput(a, il, il.sum);
   end;
   'Arr2_Count': begin 
-    ClearOutputListFromSpaces;
     var n := InputList.Count;
-    GenerateTests(10,tInt*n);
-    var il := InputListAsIntegers;
-    CheckOutputSeq(il+il.CountOf(2)+il.CountOf(3)+il.CountOf(4)+il.CountOf(5));
+    GenerateTests(10, tInt*n);
+    var a := IntArr(n);
+    CheckOutput(a, a.CountOf(2), a.CountOf(3), a.CountOf(4), a.CountOf(5));
   end;
   'Arr1_MinMax': begin 
-    ClearOutputListFromSpaces;
     var n := InputList.Count;
-    GenerateTests(10,tInt*n);
-    var il := InputListAsIntegers;
-    CheckOutputSeq(il+il.Min);
+    GenerateTests(10, tInt*n);
+    var a := IntArr(n);
+    CheckOutput(a, a.Min);
   end;
   'Arr2_MinMax': begin 
-    ClearOutputListFromSpaces;
     var n := InputList.Count;
     GenerateTests(10,tInt*n);
-    var il := InputListAsIntegers;
-    CheckOutputSeq(il+il.Max);
+    var a := IntArr(n);
+    CheckOutput(a, a.Max);
   end;
   'Arr3_MinMax': begin 
-    ClearOutputListFromSpaces;
     var n := InputList.Count;
     GenerateTests(10,tInt*n);
-    var il := InputListAsIntegers;
-    CheckOutputSeq(il+il.IndexMin);
+    var a := IntArr(n);
+    CheckOutput(a, a.IndexMin);
   end;
   'ПоискВМассиве': begin 
     var n := 10;
-    CheckData(|cInt|*n, |cInt|*n);
-    GenerateTests(10,tInt*n);
-    var a := InputListAsIntegers;
+    CheckData(cInt*n, cInt*n);
+    GenerateTests(10, tInt*n);
+    var a := IntArr(n);
     CheckOutputAfterInitial(5 in a);
   end;
   'ПоискВМассивеСтрок': begin 
@@ -152,52 +136,52 @@ begin
   end;
   'Заполнение1': begin 
     CheckData(Empty);
-    CheckOutputSeq(Arr(0,1,4,9,16,25,36,49,64,81));
+    CheckOutput(0,1,4,9,16,25,36,49,64,81);
   end;
   'Заполнение2Арифм': begin 
     CheckData(Empty);
-    CheckOutputSeq(ArrGen(10,1,x->x+2));
+    CheckOutput(ArrGen(10,1,x->x+2));
   end;
   'Заполнение3Арифм': begin 
     CheckData(Empty);
-    CheckOutputSeq(ArrGen(10,1,x->x+2));
+    CheckOutput(ArrGen(10,1,x->x+2));
   end;
   'Заполнение4Арифм': begin 
     CheckData(Empty);
-    CheckOutputSeq(ArrGen(10,1.0,x->x+0.5));
+    CheckOutput(ArrGen(10,1.0,x->x+0.5));
   end;
   'Заполнение1Геом': begin 
     CheckData(Empty);
-    CheckOutputSeq(ArrGen(10,1,x->x*2));
+    CheckOutput(ArrGen(10,1,x->x*2));
   end;
   'ЗаполнениеЧисламиФибоначчи': begin 
     CheckData(Empty);
-    CheckOutputSeq(ArrGen(10,1,1,(x,y)->x+y));
+    CheckOutput(ArrGen(10,1,1,(x,y)->x+y));
   end;
   'СоседниеЭлементы1': begin 
     var n := 10;
-    CheckData(|cInt|*n, |cInt|*n);
+    CheckData(cInt*n, cInt*n);
     GenerateTests(10,tInt*n);
     var a := IntArr(n);
     var ol := new ObjectList;
     ol.AddRange(a.Pairwise.Where(\(x,y)->x<y).Select(t->t[1]));
-    CheckOutputAfterInitialSeq(ol);
+    CheckOutputAfterInitial(ol);
   end;
   'СоседниеЭлементы2': begin 
     var n := 10;
-    CheckData(|cInt|*n, |cInt|*n);
-    GenerateTests(10,tInt*n);
-    var a := InputListAsIntegers;
+    CheckData(cInt*n, cInt*n);
+    GenerateTests(10, tInt*n);
+    var a := IntArr(n);
     var ol := new ObjectList;
     ol.AddRange(a);
     ol.Add(a.Pairwise.Where(\(x,y)->x<y).Count);
-    CheckOutputSeq(ol);
+    CheckOutput(ol);
   end;
   'ИнвертированиеМассива1': begin 
     var n := 7;
     CheckData(InitialOutput := |cStr|*n);
     var a := OutSliceStrArr(0,n-1);
-    CheckOutputSeq(a + a.Reverse);
+    CheckOutput(a + a.Reverse);
   end;
   'ИнвертированиеМассива2': begin 
     CancelMessagesIfInitial := False;
@@ -213,15 +197,16 @@ begin
     end;
     var a := IntArr(n);
     var ares := a.Reverse.ToArray;
-    CheckOutputAfterInitialSeq(ares); 
+    CheckOutputAfterInitial(ares); 
   end;
   'СтрокаКакМассив1': begin 
     CancelMessagesIfInitial := False;
     CheckData(InitialOutput := |cStr|*2);
-    CheckOutputSeq(Arr('бочка','ночка','дочка','кочка','почка','точка')); 
+    CheckOutput('бочка','ночка','дочка','кочка','почка','точка'); 
   end;
   'СтрокаКакМассив2': begin 
-    CheckOutputSeq(Arr('миг','мир','пир','пар')); 
+    CheckData(Empty);
+    CheckOutput('миг','мир','пир','пар'); 
   end;
   'СтрокаКакМассив3': begin 
     CancelMessagesIfInitial := False;
@@ -234,13 +219,13 @@ begin
     CheckOutputSeq(ol);
   end;
   'СтрокаКакМассив4': begin 
+    CheckData(Empty);
     CheckOutput(True);
   end;
   'СтрокаКакМассив5': begin 
-    CheckOutputSeq(Arr('абракадабра','эбрэкэдэбрэ'));
+    CheckData(Empty);
+    CheckOutput('абракадабра','эбрэкэдэбрэ');
   end;
-
-
   end;
 end;
 
