1e4a811e04f81089e5801e363b5744dd6466ecb9
Ivan Bondarev <ibond84@googlemail.com>
2024-10-30 20:42:59 +0100

    test for #3219

---
+++ TestSuite/nested7.pas
@@ -0,0 +1,19 @@
+﻿type
+  NewSet1<T> = class
+  end;
+
+procedure Test2;
+  var b1 := new NewSet1<integer>;
+
+  procedure Nested;
+  begin
+    assert(b1 <> nil);
+  end;
+    
+begin
+  Nested;
+end;
+
+begin
+  Test2;
+end.
