f6c9ded25c0203591e57f764f5ed650cca28fd67
MovchanGitHub <92666028+MovchanGitHub@users.noreply.github.com>
2025-03-19 12:56:08 +0300

    Update Wrong Module Name Error Positionning

--- LanguagePlugins/SPython/SyntaxTreeConverters/SPythonStandardTreeConverter/ImportToUsesVisitor.cs
+++ LanguagePlugins/SPython/SyntaxTreeConverters/SPythonStandardTreeConverter/ImportToUsesVisitor.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Security.Cryptography;
@@ -9,7 +11,7 @@ namespace Languages.SPython.Frontend.Converters
 {
     internal class ImportToUsesVisitor : BaseChangeVisitor
     {
-        HashSet<string> modulesNames = new HashSet<string>();
+        Dictionary<string, SourceContext> modulesNames = new Dictionary<string, SourceContext>();
 
         public ImportToUsesVisitor() { }
 
@@ -27,28 +29,33 @@ namespace Languages.SPython.Frontend.Converters
                 return specialModulesAliases[nameToImport];
         }
 
-        private void AddName(string name)
+        private void AddName(ident id)
         {
-            string nameToImport = GetNameToImport(name);
-            if (!modulesNames.Contains(nameToImport))
-                modulesNames.Add(nameToImport);
+            string nameToImport = GetNameToImport(id.name);
+            if (!modulesNames.ContainsKey(nameToImport))
+                modulesNames.Add(nameToImport, id.source_context);
         }
 
         public override void visit(import_statement _import_statement)
         {
             foreach (as_statement as_Statement in _import_statement.modules_names.as_statements)
-                AddName(as_Statement.real_name.name);
+                AddName(as_Statement.real_name);
         }
 
         public override void visit(from_import_statement _from_import_statement)
         {
-            AddName(_from_import_statement.module_name.name);
+            AddName(_from_import_statement.module_name);
         }
 
         public void AddUsesToUsesList(uses_list _uses_list)
         {
-            foreach (string module_name in modulesNames)
-                _uses_list.AddUsesList(new uses_list(module_name));
+            foreach (string module_name in modulesNames.Keys)
+            {
+                SourceContext sc = modulesNames[module_name];
+                uses_list us = new uses_list(new unit_or_namespace(module_name, sc), sc);
+                us[0].source_context = sc;
+                _uses_list.AddUsesList(us, sc);
+            }
         }
 
         public override void Exit(syntax_tree_node stn)
