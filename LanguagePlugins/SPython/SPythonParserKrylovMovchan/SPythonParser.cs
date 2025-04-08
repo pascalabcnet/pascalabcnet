3d9ea9639ea0f19c0c210b9f4b61895691bf2729
AlexanderZemlyak <zemlyaksanya@mail.ru>
2025-04-08 10:55:51 +0300

    Add implementation for parser methods used by Intellisense

--- LanguagePlugins/SPython/SPythonParserKrylovMovchan/SPythonParser.cs
+++ LanguagePlugins/SPython/SPythonParserKrylovMovchan/SPythonParser.cs
@@ -3,76 +3,15 @@ using System.Collections.Generic;
 using PascalABCCompiler.SyntaxTree;
 using PascalABCCompiler.Parsers;
 using SPythonParserYacc;
+using System;
 
 
 
 namespace SPythonParser
 {
-    /*public class SPythonGPPGParserHelper
-    {
-        private List<Error> Errs;
-        private List<CompilerWarning> Warnings;
-        private string FileName;
-        public bool build_tree_for_formatter = false;
-        public List<string> DefinesList = null;
-
-        public static Func<bool> CheckIfParsingUnit;
-
-        public SPythonGPPGParserHelper(List<Error> Errs, List<CompilerWarning> Warnings, string FileName)
-        {
-            this.Errs = Errs;
-            this.Warnings = Warnings;
-            this.FileName = FileName;
-        }
-
-        public syntax_tree_node Parse(string Text, List<compiler_directive> compilerDirectives = null)
-        {
-#if DEBUG
-#if _ERR
-            FileInfo f = new FileInfo(FileName);
-            var sv = Path.ChangeExtension(FileName,".grmtrack1");
-            var sw = new StreamWriter(sv);
-            Console.SetError(sw);
-#endif
-#endif
-            SPythonParserTools parsertools = new SPythonParserTools(); // контекст сканера и парсера
-            parsertools.errors = Errs;
-            parsertools.warnings = Warnings;
-            parsertools.compilerDirectives = compilerDirectives;
-            parsertools.CurrentFileName = Path.GetFullPath(FileName);
-            
-
-            IndentArranger ia = new IndentArranger();
-            ia.ProcessSourceText(ref Text);
-
-            var scanner = new Scanner();
-            scanner.SetSource(Text, 0);
-            scanner.parsertools = parsertools;// передали parsertools в объект сканера
-            if (DefinesList != null)
-                scanner.Defines.AddRange(DefinesList);
-
-            SPythonGPPGParser parser = new SPythonGPPGParser(scanner);
-            parsertools.build_tree_for_formatter = build_tree_for_formatter;
-
-            parser.is_unit_to_be_parsed = CheckIfParsingUnit.Invoke();
-
-            parser.parsertools = parsertools; // передали parsertools в объект парсера
-
-            if (!parser.Parse())
-                if (Errs.Count == 0)
-                    parsertools.AddErrorFromResource("UNEXPECTED_SYNTAX_ERROR", null);
-#if DEBUG
-#if _ERR
-            sw.Close();
-#endif
-#endif
-            return parser.root;
-        }
-    }*/
 
     public class SPythonLanguageParser : BaseParser
     {
-        //private SPythonGPPGParserHelper localparserhelper;
 
         public SPythonLanguageParser()
         {
@@ -89,9 +28,6 @@ namespace SPythonParser
         protected override void PreBuildTree(string FileName)
         {
             CompilerDirectives = new List<compiler_directive>();
-
-            // чтобы строки во всех программах индексировались с нуля | Movchan 18.05.24
-            CompilerDirectives.Add(new compiler_directive(new token_info("zerobasedstrings"), new token_info("")));
 
         }
 
@@ -145,27 +81,40 @@ namespace SPythonParser
 
         protected override syntax_tree_node BuildTreeInTypeExprMode(string FileName, string Text)
         {
-            return null;
+            Text = string.Concat("<<type>>", Environment.NewLine, Text);
+
+            return Parse(Text, FileName) as expression;
         }
 
         protected override syntax_tree_node BuildTreeInExprMode(string FileName, string Text)
         {
-            return null;
+            if (Text == string.Empty)
+                return null;
+            
+            Text = string.Concat("<<expression>>", Environment.NewLine, Text);
+
+            return Parse(Text, FileName) as expression;
         }
 
         protected override syntax_tree_node BuildTreeInSpecialMode(string FileName, string Text)
         {
-            return null;
+            Errors.Clear();
+            
+            return Parse(Text, FileName);
         }
 
         protected override syntax_tree_node BuildTreeInFormatterMode(string FileName, string Text)
         {
-            return null;
+            Errors.Clear();
+            
+            return Parse(Text, FileName, true);
         }
 
         protected override syntax_tree_node BuildTreeInStatementMode(string FileName, string Text)
         {
-            return null;
+            Text = string.Concat("<<statement>>", Environment.NewLine, Text);
+
+            return Parse(Text, FileName) as statement;
         }
     }
 }
