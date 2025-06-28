782945e45ad331c5d54fa28884c4be2ef783eb9d
SamuraiGH <anonimajzer3@mail.ru>
2025-06-28 14:41:43 +0300

    removing hard code

---
+++ NETGenerator/TypeExt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PascalABCCompiler.NETGenerator
+{
+	internal static class TypeExt
+	{
+		/// <summary>
+		/// Повторяет работу свойства System.Type.IsConstructedGenericType из net fx 4.5
+		/// </summary>
+		public static bool IsConstructedGenericType(this Type t)
+		{
+			return t.IsGenericType && !t.IsGenericTypeDefinition;
+		}
+	}
+}
