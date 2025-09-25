8dbc97a82276921fc62b50d220bb9dd6ea735316
Mikhalkovich Stanislav <miks@math.sfedu.ru>
2025-09-25 20:15:40 +0300

    ABCObjects Linux

--- _GenerateLinuxVersion.bat
+++ _GenerateLinuxVersion.bat
@@ -235,7 +235,7 @@ xcopy InstallerSamples\StandardUnits\       Release\Samples\StandardUnits\      
 xcopy InstallerSamples\WhatsNew\            Release\Samples\WhatsNew\            /s /e
 
 xcopy InstallerSamples\Graphics\GraphABC\   Release\Samples\Graphics\GraphABC\   /s /e
-xcopy InstallerSamples\Graphics\GraphABC\ABCObjects\   Release\Samples\Graphics\GraphABC\ABCObjects\   /s /e
+xcopy InstallerSamples\Graphics\ABCObjects\   Release\Samples\Graphics\ABCObjects\   /s /e
 
 cd Release
 
