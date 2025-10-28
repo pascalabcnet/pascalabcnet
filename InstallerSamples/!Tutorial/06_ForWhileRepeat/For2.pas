c32953091512f6efc1bf74a05b0463d0b9c9ba42
Mikhalkovich Stanislav <miks@math.sfedu.ru>
2025-10-28 10:19:43 +0300

    Правка примеров

--- InstallerSamples/!Tutorial/06_ForWhileRepeat/For2.pas
+++ InstallerSamples/!Tutorial/06_ForWhileRepeat/For2.pas
@@ -1,5 +1,4 @@
 ﻿// Цикл for. Последовательность случайных чисел
-
 const n = 20;
 
 begin
@@ -7,6 +6,7 @@ begin
   for var i := 1 to n do
     Print(Random(2, 5));
   Println;
+  
   Print('Случайные целые от 1 до 99:');
   for var i := n downto 1 do
     Print(Random(1, 99));
