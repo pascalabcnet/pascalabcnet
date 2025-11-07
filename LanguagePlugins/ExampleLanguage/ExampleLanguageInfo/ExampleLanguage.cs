157f9aca4acdaa6c2a0f4ab0464c1742a6a08bc9
AlexanderZemlyak <zemlyaksanya@mail.ru>
2025-11-07 19:59:17 +0300

    Implement minimal language plugin example

---
+++ LanguagePlugins/ExampleLanguage/ExampleLanguageInfo/ExampleLanguage.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using Languages.Facade;
+using PascalABCCompiler.SyntaxTreeConverters;
+using PascalABCCompiler.SystemLibrary;
+using PascalABCCompiler.TreeConverter;
+
+namespace Languages.Example
+{
+    /// <summary>
+    /// Демонстрационный язык.
+    /// Программа на нем может содержать только:
+    /// 1) Присваивания целых констант
+    /// 2) Вызов функций модуля PABCSystem с передачей им параметром одной константы или переменной
+    /// </summary>
+    public class ExampleLanguage : BaseLanguage
+    {
+        public ExampleLanguage() : base(
+
+            languageInformation: new Frontend.Data.ExampleLanguageInformation(),
+            parser: new Frontend.Wrapping.ExampleLanguageParser(),
+            docParser: null,
+
+            syntaxTreeConverters: new List<ISyntaxTreeConverter>() { new DefaultSyntaxTreeConverter() }
+            )
+        { }
+
+        public override void SetSemanticConstants()
+        {
+            SemanticRulesConstants.ClassBaseType = SystemLibrary.object_type;
+            SemanticRulesConstants.StructBaseType = SystemLibrary.value_type;
+            SemanticRulesConstants.AddResultVariable = true;
+            SemanticRulesConstants.ZeroBasedStrings = true;
+            SemanticRulesConstants.FastStrings = false;
+            SemanticRulesConstants.InitStringAsEmptyString = true;
+            SemanticRulesConstants.UseDivisionAssignmentOperatorsForIntegerTypes = false;
+            SemanticRulesConstants.ManyVariablesOneInitializator = false;
+            SemanticRulesConstants.OrderIndependedMethodNames = true;
+            SemanticRulesConstants.OrderIndependedFunctionNames = false;
+            SemanticRulesConstants.OrderIndependedTypeNames = false;
+            SemanticRulesConstants.EnableExitProcedure = true;
+            SemanticRulesConstants.StrongPointersTypeCheckForDotNet = true;
+            SemanticRulesConstants.AllowChangeLoopVariable = false;
+            SemanticRulesConstants.AllowGlobalVisibilityForPABCDll = true;
+            SemanticRulesConstants.AllowMethodCallsWithoutParentheses = false;
+        }
+
+        public override void SetSyntaxTreeToSemanticTreeConverter()
+        {
+            // используем конвертор главного языка платформы (PascalABC.NET)
+            SyntaxTreeToSemanticTreeConverter = LanguageProvider.Instance.MainLanguage.SyntaxTreeToSemanticTreeConverter;
+        }
+
+    }
+}
---
+++ LanguagePlugins/ExampleLanguage/ExampleLanguageParser/ExampleParserTools.cs
@@ -0,0 +1,49 @@
+﻿using PascalABCCompiler.Errors;
+using PascalABCCompiler.ParserTools;
+using PascalABCCompiler.ParserTools.Directives;
+using PascalABCCompiler.SyntaxTree;
+using System;
+using System.Collections.Generic;
+
+namespace Languages.Example.Frontend.Core
+{
+    internal class ExampleParserTools : BaseParserTools
+    {
+        public override Dictionary<string, string> TokenNum { get; protected set; }
+
+        private void InitializeTokenNum()
+        {
+            TokenNum = new Dictionary<string, string>();
+        }
+
+        public ExampleParserTools(List<Error> errors, List<CompilerWarning> warnings,
+            Dictionary<string, DirectiveInfo> validDirectives, bool buildTreeForFormatter = false, bool buildTreeForFormatterStrings = false,
+            string currentFileName = null, List<compiler_directive> compilerDirectives = null)
+            : base(errors, warnings, validDirectives, buildTreeForFormatter, buildTreeForFormatterStrings, currentFileName, compilerDirectives)
+        {
+            InitializeTokenNum();
+        }
+
+        protected override string GetFromStringResources(string id)
+        {
+            return BaseStringResources.Get(id, "EXAMPLEPARSER_");
+        }
+
+        // методы ниже не понадобятся для минимальной реализации
+
+        public override literal create_string_const(string text, SourceContext sc)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string ExtractDirectiveTextWithoutSpecialSymbols(string directive)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string ReplaceSpecialSymbols(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
