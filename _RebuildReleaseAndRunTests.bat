1aa1b02ccf0a612c004f0df3d04dc1866b5fc9d6
AlexanderZemlyak <zemlyaksanya@mail.ru>
2026-01-02 18:36:46 +0300

    Add RebuildStandartModules SPython.pas compilation in bat files

--- _RebuildReleaseAndRunTests.bat
+++ _RebuildReleaseAndRunTests.bat
@@ -4,6 +4,7 @@ dotnet build -c Release --no-incremental PascalABCNET.sln
 
 cd ReleaseGenerators
 ..\bin\pabcnetc RebuildStandartModules.pas /rebuild
+..\bin\pabcnetc RebuildStandartModulesSPython.pas /rebuild
 @IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 
@@ -20,6 +21,7 @@ ExecHide.exe gacutil.exe /u PABCRtl
 ExecHide.exe gacutil.exe /i ..\bin\Lib\PABCRtl.dll
 
 ..\bin\pabcnetc RebuildStandartModules.pas /rebuild
+..\bin\pabcnetc RebuildStandartModulesSPython.pas /rebuild
 @IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 
