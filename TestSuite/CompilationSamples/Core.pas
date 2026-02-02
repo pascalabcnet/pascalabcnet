141e8074266a102671bda850d7e271f594fa9ee1
AlexanderZemlyak <zemlyaksanya@mail.ru>
2026-02-02 15:00:59 +0300

    Delete PascalABC.NET Lib files from CompilationSamples

--- TestSuite/CompilationSamples/Core.pas
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Ivan Bondarev, Stanislav Mikhalkovich (for details please see \doc\copyright.txt)
-// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
-unit Core;
-{$reference 'System.Core.dll'}
-end.
--- TestSuite/CompilationSamples/Events.pas
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Ivan Bondarev, Stanislav Mikhalkovich (for details please see \doc\copyright.txt)
-// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
-// Для совместимости с Pascal ABC
-unit Events;
-end.
