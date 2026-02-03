d9697b8d84e0f7c224793f9ba3e5f3ac37f62f97
Mikhalkovich Stanislav <miks@math.sfedu.ru>
2026-02-03 10:18:06 +0300

    DataFrameExample in Whats new

---
+++ InstallerSamples/WhatsNew/3_11_1/DataFrameABC1.pas
@@ -0,0 +1,21 @@
+﻿// Новый стандартный модуль для работы с датасетами
+uses DataFrameABC;
+
+begin
+  var df := DataFrame.FromCsvText('''
+  name,age,score
+  Alice,20,85
+  Bob,22,90
+  Charlie,21,78
+  Bob,22,90
+  Clara,,78
+  Kat,21,NA
+  ''');
+  
+  df.Filter(row -> row.Int('age') > 20).Println;
+  
+  df.GroupBy('age').Mean('score').Println;
+
+  var stat := df.Describe('score');
+  Println($'score: count={stat.Count}, min={stat.Min}, max={stat.Max}, mean={stat.Mean}, std={stat.Std.Round(3)}');
+end.
