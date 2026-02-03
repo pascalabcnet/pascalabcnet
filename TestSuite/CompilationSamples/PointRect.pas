137c0599afa84b85feb1424831376891ab9212df
Александр Земляк <92867056+AlexanderZemlyak@users.noreply.github.com>
2026-02-03 09:52:33 +0300

    Удаление Lib файлов из CompilationSamples (#3385)
    * Delete PascalABC.NET Lib files from CompilationSamples
    
    * Delete SPython Lib files from CompilationSamples
    
    * Delete DataFrameABC from CompilationSamples

--- TestSuite/CompilationSamples/PointRect.pas
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Ivan Bondarev, Stanislav Mikhalkovich (for details please see \doc\copyright.txt)
-// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
-unit PointRect;
-
-//Для совместимости с Pascal ABC
-
-end.
