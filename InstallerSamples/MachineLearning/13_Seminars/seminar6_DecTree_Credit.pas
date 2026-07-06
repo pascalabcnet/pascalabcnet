42b1f21a8041b76b28f7f28b90f1f3c00d2f30db
Mikhalkovich Stanislav <miks@math.sfedu.ru>
2026-07-06 17:25:30 +0300

    seminar6_DecTree_Credit.pas

---
+++ InstallerSamples/MachineLearning/13_Seminars/seminar6_DecTree_Credit.pas
@@ -0,0 +1,39 @@
+﻿uses MLABC;
+
+begin
+  var df := DataFrame.FromCsvText('''
+Возраст,Зарплата,Одобрить кредит
+17,25,Отказать
+64,80,Одобрить
+18,22,Отказать
+20,36,Одобрить
+38,37,Отказать
+49,59,Одобрить
+55,74,Одобрить
+25,70,Отказать
+29,33,Отказать
+31,102,Одобрить
+33,88,Отказать
+''');
+
+  var featureNames := ['Возраст', 'Зарплата'];
+  var X := df.ToMatrix(featureNames);
+  var target := df.EncodeTarget('Одобрить кредит');
+  
+  var model := new DecisionTreeClassifier;
+  model.Fit(X, target.Labels);
+  
+  var view := model.Tree(featureNames, target.ClassNames);
+  
+  // Вывод дерева решений
+  Println(view);
+
+  // Новый клиент
+  var example := [20.0, 100.0];
+
+  var pred := model.PredictOne(example);
+
+  Println('Возраст:', example[0]);
+  Println('Зарплата:', example[1]);
+  Println('Одобрить кредит:', target.ClassNames[pred]);
+end.
